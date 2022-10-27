e85a745da562d27c8e5307d5921b410ab28c2517
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-10-27 15:51:57 +0300

    Auth manager bug fixed

--- SpotifyClone/Controllers/SubPages/AuthViewController.swift
+++ SpotifyClone/Controllers/SubPages/AuthViewController.swift
@@ -32,7 +32,7 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         view.backgroundColor = .systemBackground
         webView.navigationDelegate = self
         view.addSubview(webView)
-        guard let url = AuthManager.shared.singInUrl else {
+        guard let url = AuthManager.shared.signInURL else {
             return
         }
         webView.load(URLRequest(url: url))
