e2655a4fd754245f694459e1a0fb93d0e8a6a914
Mustafa Çiçek <f191220093@ktun.edu.tr>
2022-10-28 01:47:07 +0300

    app icon created.

--- SpotifyClone/Resources/AppDelegate.swift
+++ SpotifyClone/Resources/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let window = UIWindow(frame: UIScreen.main.bounds)
         
         if AuthManager.shared.isSignedIn {
+            // no needed token
+            AuthManager.shared.refreshIfNeeded(completion: nil)
             window.rootViewController = TabBarViewController()
         }
         else {
